c0c01bc9af5957fdca36c7f0ac6a7b02f4bea8d2
Sanh Vu <sanhvu020@gmail.com>
2022-08-11 23:21:00 -0400

    commit tree 1f066653d88521e75c98388c04f03ed05ae372ea

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a , b) {
+    return a + b;
+}
+function subtract(a , b) {
+    return a - b;
+}
+function multiply(a , b) {
+     return a * b;
+}
+function divide(a , b) {
+    return a / b;
+}
+function increment(n) {
+    return n+= 1;
+    n;
+}
+function decrement(n) {
+    return n-= 1;
+    n;
+}
+function makeInt(n , base) {
+    const parsed = parseInt(n , 10);
+    return parsed;
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
